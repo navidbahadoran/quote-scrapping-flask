1bcf5e67870b830c43709cd69c43479a1fd0f506
Navid Bahadoran <navid.bahadoran@gmail.com>
2019-07-20 07:27:18 -0700

    final

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+#!/usr/bin/env python
+from flask import Flask
 from bs4 import BeautifulSoup
 import requests
+import os
 
-r = requests.get('http://www.quotationspage.com/random.php').content
-soup = BeautifulSoup(r, 'lxml')
-a = soup.find_all('dt', class_='quote')
-print(a[0].getText())
+app = Flask(__name__)
 
 
+def get_quote():
+    response = requests.get('http://www.quotationspage.com/random.php').content
+    soup = BeautifulSoup(response, 'lxml')
+    quote = soup.find_all('dt', class_='quote')[0].text
+    return quote
+
+
+@app.route('/')
+def home():
+    return get_quote()
+
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
